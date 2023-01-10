0112c4cf8c5999cca03fbd95d2e2e12b67e76abe
Marco <posta.email3001@gmail.com>
2023-01-10 15:50:19 +0100

    stampati hotels con tutti i dati disponibili.

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Document</title>
+
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+    ?>
+</head>
+<body>
+    
+    <?php
+
+        foreach ($hotels as $hotel) {
+            echo "<h1>" . "Nome: " . $hotel["name"] . "</h1>"
+            . "<h3>" . "Descrizione: ". $hotel["description"] . "</h3>"
+            . "<h3>" . "Parcheggio: ". $hotel["parking"] . "</h3>"
+            . "<h3>" . "Voto: ". $hotel["vote"] . "</h3>"
+            . "<h3>" . "Distanza dal centro: ". $hotel["distance_to_center"] . "</h3>";
+        };
+
+    ?>
+    
+</body>
+</html>
